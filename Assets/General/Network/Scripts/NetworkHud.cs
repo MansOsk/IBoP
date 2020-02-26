5157d529c6cf137b4bd726ff6d6360e32dff476d
ojawez-7 <36762144+ojawez-7@users.noreply.github.com>
2020-02-26 10:56:25 +0100

    Push

---
+++ Assets/General/Network/Scripts/NetworkHud.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+using UnityEngine.Networking.Match;
+using UnityEngine.Networking.PlayerConnection;
+using UnityEngine.Networking.Types;
+
+public class NetworkHud : MonoBehaviour
+{
+    NetworkManager m;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m = gameObject.GetComponent<NetworkManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Host()
+    {
+        m.StartHost();
+    }
+    public void Join()
+    {
+        m.StartClient();
+    }
+    public void ChangeIp(string ip)
+    {
+        m.networkAddress = ip;
+    }
+}

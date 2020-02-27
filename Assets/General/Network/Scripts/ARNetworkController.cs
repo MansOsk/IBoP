9d8a52c9d8e46e8d24bae27c205f460fab9c191a
ojawez-7 <36762144+ojawez-7@users.noreply.github.com>
2020-02-27 14:45:01 +0100

    NetworkARController

---
+++ Assets/General/Network/Scripts/ARNetworkController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using GoogleARCore;
+using UnityEngine;
+using UnityEngine.UI;
+using GoogleARCore.Examples.AugmentedImage;
+using UnityEngine.Networking;
+
+/// <summary>
+/// Controller for AugmentedImage example.
+/// </summary>
+/// <remarks>
+/// In this sample, we assume all images are static or moving slowly with
+/// a large occupation of the screen. If the target is actively moving,
+/// we recommend to check <see cref="AugmentedImage.TrackingMethod"/> and
+/// render only when the tracking method equals to
+/// <see cref="AugmentedImageTrackingMethod"/>.<c>FullTracking</c>.
+/// See details in <a href="https://developers.google.com/ar/develop/c/augmented-images/">
+/// Recognize and Augment Images</a>
+/// </remarks>
+public class ARNetworkController : ARController
+{
+    /// <summary>
+    /// The Unity Awake() method.
+    /// </summary>
+    public void Awake()
+    {
+        // Enable ARCore to target 60fps camera capture frame rate on supported devices.
+        // Note, Application.targetFrameRate is ignored when QualitySettings.vSyncCount != 0.
+        Application.targetFrameRate = 60;
+    }
+
+    /// <summary>
+    /// The Unity Update method.
+    /// </summary>
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public override void ShowObject(AugmentedImage image, out ARVisualizer visualizer)
+    {
+        visualizer = null;
+        if (NetworkServer.active)
+        {
+            base.ShowObject(image, out visualizer);
+            NetworkServer.Spawn(visualizer.gameObject);
+        }
+    }
+}

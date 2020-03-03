800205eddfc0c48e2c9fe49ea572870541831e9d
mans <mans.oskarsson98@outlook.com>
2020-03-03 16:01:08 +0100

    Fixed instansiate for popup and general fixes

---
+++ Assets/Testing/Multiplayer/Resources/Scripts/NetworkPopup.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkPopup : PopUp
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Created = false; 
+    }
+
+    void Update()
+    {
+        if(State != null)
+            ProcessGameState(State.GetState());
+    }
+
+    public override void ProcessGameState(int[] state)
+    {
+        if (state[0] > -1 && (!TriggerOnce || !Created))
+        {
+            foreach (GameObject popUp in PopUps)
+            {
+                CreateObject(popUp);
+            }
+        }
+    }
+
+    public override GameObject CreateObject(GameObject popUp)
+    {
+        var p = NetworkManager.Instantiate(popUp);
+        NetworkServer.Spawn(p);
+
+        Created = true;
+
+        return p;
+    }
+}
